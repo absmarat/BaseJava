e540894ca4249d6e61e5253254a28be79b57d753
MaratAbs <absatdarov43@gmail.com>
2026-08-22 14:41:49 +0700

    Дал переменным r1, r2, r3 более осмысленные имена: resume1, resume2, resume3

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -5,24 +5,24 @@ public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
-        r1.uuid = "uuid1";
-        Resume r2 = new Resume();
-        r2.uuid = "uuid2";
-        Resume r3 = new Resume();
-        r3.uuid = "uuid3";
+        Resume resume1 = new Resume();
+        resume1.uuid = "uuid1";
+        Resume resume2 = new Resume();
+        resume2.uuid = "uuid2";
+        Resume resume3 = new Resume();
+        resume3.uuid = "uuid3";
 
-        ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r2);
-        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(resume1);
+        ARRAY_STORAGE.save(resume2);
+        ARRAY_STORAGE.save(resume3);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
+        System.out.println("Get resume1: " + ARRAY_STORAGE.get(resume1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.uuid);
+        ARRAY_STORAGE.delete(resume1.uuid);
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
