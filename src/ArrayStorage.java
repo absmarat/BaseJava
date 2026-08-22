62a00217d29e2fcd28e6e4ed92c9ebe4bd02ff95
MaratAbs <absatdarov43@gmail.com>
2026-08-22 14:26:54 +0700

    Реализовал методы save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size;
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (resume == null) return;
+        if (resume.uuid == null || resume.uuid.isBlank()) return;
+        if (size >= storage.length) return;
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(resume.uuid)) {
+                storage[i] = resume;
+                return;
+            }
+        }
+        storage[size++] = resume;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                if (i < size - 1) {
+                    System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                }
+                storage[--size] = null;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
