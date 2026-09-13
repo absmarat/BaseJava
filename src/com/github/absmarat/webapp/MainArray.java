8660322fd55460394743e8926675c2aabff97fbd
MaratAbs <absatdarov43@gmail.com>
2026-09-13 17:49:04 +0700

    lesson_2: поместил класс в пакет

--- src/com/github/absmarat/webapp/MainArray.java
+++ src/com/github/absmarat/webapp/MainArray.java
@@ -1,4 +1,4 @@
-package com.github.absmarat;
+package com.github.absmarat.webapp;
 
 import com.github.absmarat.webapp.model.Resume;
 import com.github.absmarat.webapp.storage.ArrayStorage;
@@ -37,7 +37,7 @@ public class MainArray {
                     break;
                 case "save":
                     resume = new Resume();
-                    resume.uuid = uuid;
+                    resume.setUuid(uuid);
                     ARRAY_STORAGE.save(resume);
                     printAll();
                     break;
